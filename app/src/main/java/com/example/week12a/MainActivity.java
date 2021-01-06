15ddb4a5cc56f512425d605e64d4f1322a32fba5
ostandmad <shashankpunna0@gmail.com>
2021-01-06 11:59:04 +0530

    Initial commit

---
+++ app/src/main/java/com/example/week12a/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.week12a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+public class MainActivity extends Activity {
+    EditText editTextTo, editTextSubject, editTextMessage;
+    Button send;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editTextTo = (EditText) findViewById(R.id.editTextTextPersonName);
+        editTextSubject = (EditText) findViewById(R.id.editTextTextPersonName2);
+        editTextMessage = (EditText) findViewById(R.id.editTextTextPersonName3);
+        send = (Button) findViewById(R.id.button);
+        send.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                String to = editTextTo.getText().toString();
+                String subject = editTextSubject.getText().toString();
+                String message = editTextMessage.getText().toString();
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.putExtra(Intent.EXTRA_EMAIL, new String[]{to});
+                email.putExtra(Intent.EXTRA_SUBJECT, subject);
+                email.putExtra(Intent.EXTRA_TEXT, message);
+                email.setData(Uri.parse("mailto:"));
+                email.setType("message/rfc822");
+                startActivity(Intent.createChooser(email, "Choose an Email client :"));
+            }
+        });
+    }
+}
